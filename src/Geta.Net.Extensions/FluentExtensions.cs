0af8ec5e956d6807647b342e6b42bc37c9bb5ddd
Māris Krivtežs <marisks@gmail.com>
2016-10-19 17:39:53 +0300

    Added fluent extensions.
    Added useful extensions to improve fluent APIs. These changes fixes
    https://github.com/Geta/EPi.Extensions/issues/12

---
+++ src/Geta.Net.Extensions/FluentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Geta.Net.Extensions
+{
+    public static class FluentExtensions
+    {
+        public static T If<T>(this T source, bool condition, Func<T, T> func)
+        {
+            return source.If(_ => condition, func);
+        }
+
+        public static T If<T>(this T source, Func<bool> condition, Func<T, T> func)
+        {
+            return source.If(_ => condition(), func);
+        }
+
+        public static T If<T>(this T source, Func<T, bool> condition, Func<T, T> func)
+        {
+            return condition(source) ? func(source) : source;
+        }
+
+        public static T Fluent<T>(this T source, Action<T> action)
+        {
+            action(source);
+            return source;
+        }
+    }
+}
