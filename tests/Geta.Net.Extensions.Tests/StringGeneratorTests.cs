1ef8ad2c19b94e52a370e6d790a2afe7a848927e
Kaspars Ozols <kaspars.ozols.private@gmail.com>
2017-05-25 14:33:02 +0300

    Added unit test for #1

---
+++ tests/Geta.Net.Extensions.Tests/StringGeneratorTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using Geta.Net.Extensions.Generators;
+using Xunit;
+
+namespace Geta.Net.Extensions.Tests
+{
+    public class StringGeneratorTests
+    {
+        [Theory]
+        [InlineData(1, 1, 1, 1)]
+        [InlineData(10, 0, 0, 0)]
+        [InlineData(0, 10, 0, 0)]
+        [InlineData(0, 0, 10, 0)]
+        [InlineData(0, 0, 0, 10)]
+        [InlineData(5, 5, 5, 5)]
+        public void String_generation_includes_all_required_chars(int expectedUppercaseCharCount,
+            int expectedLowerCaseCharCount, int expectedDigitCount, int expectedSymbolCount)
+        {
+            var value = StringGenerator.GenerateRandomString(expectedUppercaseCharCount, expectedLowerCaseCharCount,
+                expectedDigitCount, expectedSymbolCount);
+
+            const string allLowercaseChars = "abcdefghijkmnopqrstuvwxyz";
+            const string allUpperCaseChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
+            const string allDigits = "0123456789";
+            const string allSymbols = "!&%$*";
+
+            var lowercaseCharCount = value.Count(x => allLowercaseChars.Contains(x));
+            var uppercaseCharCount = value.Count(x => allUpperCaseChars.Contains(x));
+            var digitCount = value.Count(x => allDigits.Contains(x));
+            var symbolCount = value.Count(x => allSymbols.Contains(x));
+            var totalCount = value.Length;
+
+            Assert.Equal(expectedLowerCaseCharCount, lowercaseCharCount);
+            Assert.Equal(expectedUppercaseCharCount, uppercaseCharCount);
+            Assert.Equal(expectedDigitCount, digitCount);
+            Assert.Equal(expectedSymbolCount, symbolCount);
+
+            var expectedTotalCount = expectedUppercaseCharCount + expectedLowerCaseCharCount + expectedDigitCount +
+                                     expectedSymbolCount;
+
+            Assert.Equal(expectedTotalCount, totalCount);
+        }
+
+        [Fact]
+        public void String_generation_is_random()
+        {
+            var values = Enumerable.Range(1, 100)
+                .Select(x => StringGenerator.GenerateRandomString(5, 5, 5, 5))
+                .ToList();
+
+            var firstDuplicate = values
+                .GroupBy(x => x)
+                .Where(x => x.Count() > 1)
+                .Select(x => x.Key)
+                .FirstOrDefault();
+
+            Assert.Null(firstDuplicate);
+        }
+    }
+}
