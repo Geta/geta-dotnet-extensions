4a078916bebc1f958118018c86b15df0b719044e
Frederik Vig <frederik@geta.no>
2017-09-20 14:57:28 -0400

    New DateTime extension methods for Unix Epoch time

---
+++ src/Geta.Net.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Geta.Net.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0);
+
+        /// <summary>
+        /// Returns a Unix Epoch time given a <see cref="DateTime"/>.
+        /// </summary>
+        /// <param name="dateTime">The <see cref="DateTime"/> to convert.</param>
+        public static long ToEpochTime(this DateTime dateTime) => (long) (dateTime - Epoch).TotalSeconds;
+
+        /// <summary>
+        /// Returns a Unix Epoch time if given a value, and null otherwise.
+        /// </summary>
+        /// <param name="dateTime">The <see cref="DateTime"/> to convert.</param>
+        /// <returns></returns>
+        public static long? ToEpochTime(this DateTime? dateTime) => dateTime.HasValue ? (long?) ToEpochTime(dateTime) : null;
+    }
+}
