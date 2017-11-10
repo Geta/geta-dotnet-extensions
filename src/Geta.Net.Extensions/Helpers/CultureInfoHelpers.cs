5ddf129efda32d100a8999d8a94a45d3f5d35688
Māris Krivtežs <marisks@gmail.com>
2017-11-10 15:53:41 +0200

    Added culture info helper to check if culture exists by the name.

---
+++ src/Geta.Net.Extensions/Helpers/CultureInfoHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Geta.Net.Extensions.Helpers
+{
+    public static class CultureInfoHelpers
+    {
+        /// <summary>
+        /// Checks if there is a culture by the provided name.
+        /// </summary>
+        /// <param name="cultureName">Culture name to find.</param>
+        /// <returns>Returns true if culture exists and false if not.</returns>
+        public static bool Exists(string cultureName)
+        {
+            return CultureInfo.GetCultures(CultureTypes.AllCultures)
+                    .Any(culture => string.Equals(culture.Name, cultureName, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
